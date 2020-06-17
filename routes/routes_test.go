e9a16dd979460d1205f37f44f9e6f6be9829c3de
vidhlakh <vidhlakh@gmail.com>
2020-06-17 12:35:46 -0400

    Update testcases for router.go

--- routes/routes_test.go
+++ routes/routes_test.go
@@ -2,6 +2,7 @@
 package routes
 
 import (
+	"go-gorm-gin/routes"
 	"reflect"
 	"testing"
 
@@ -9,16 +10,33 @@ import (
 )
 
 func TestSetupRouter(t *testing.T) {
+
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+	// grp1 := r.Group("/todolist")
+	// {
+	// 	grp1.GET("task", controllers.GetAllTasks)
+	// 	grp1.POST("task", controllers.CreateTask)
+	// 	grp1.GET("task/:id", controllers.GetTaskByID)
+	// 	//grp1.GET("task/:taskstatus", controllers.GetTaskByTaskstatus)
+	// 	grp1.PUT("task/:id", controllers.UpdateTask)
+	// 	grp1.DELETE("task/:id", controllers.DeleteTask)
+	// }
+
 	tests := []struct {
 		name string
 		want *gin.Engine
 	}{
-		// TODO: Add test cases.
-
+		{
+			name: "Test Router",
+			want: r,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := SetupRouter(); !reflect.DeepEqual(got, tt.want) {
+			got := routes.SetupRouter()
+
+			if reflect.TypeOf(got) != reflect.TypeOf(tt.want) {
 				t.Errorf("SetupRouter() = %v, want %v", got, tt.want)
 			}
 		})
